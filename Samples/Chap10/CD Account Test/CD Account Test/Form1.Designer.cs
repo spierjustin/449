03e1e6854871234dde6157cd2a73b5bb8d90d096
Justin Spier <Z1771550@students.niu.edu>
2019-04-23 18:12:45 -0500

    Revert "Updated Tutorial 10.1"
    This reverts commit 046efe1daf4b5a3cecf08c7ec3c7b0669fccbf01.

--- Samples/Chap10/CD Account Test/CD Account Test/Form1.Designer.cs
+++ Samples/Chap10/CD Account Test/CD Account Test/Form1.Designer.cs
@@ -60,7 +60,6 @@
             this.exitButton.TabIndex = 7;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = true;
-            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // createObjectButton
             // 
@@ -70,7 +69,6 @@
             this.createObjectButton.TabIndex = 6;
             this.createObjectButton.Text = "Create Object";
             this.createObjectButton.UseVisualStyleBackColor = true;
-            this.createObjectButton.Click += new System.EventHandler(this.createObjectButton_Click);
             // 
             // objectGroupBox
             // 
