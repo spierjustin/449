d3700bbc42dad7e3cac2ef7b69800f0808201c83
Justin Spier <Z1771550@students.niu.edu>
2019-04-23 20:57:01 -0500

    Completed Tutorial 10.3 in the book

---
+++ Samples/Chap10/Computer Science Student/Computer Science Student/CompSciStudent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer_Science_Student
+{
+    class CompSciStudent : Student
+    {
+        //constants
+        private double MATH_HOURS = 20;
+        private double CS_HOURS = 40;
+        private double GEN_HOURS = 60;
+
+        //fields
+        private string _academicTrack;
+
+        //Constructor
+        public CompSciStudent(string name, string id, string track) : base (name, id)
+        {
+            _academicTrack = track;
+        }
+
+        //AcademicTrack Property
+        public string AcademicTrack
+        {
+            get { return _academicTrack; }
+            set { _academicTrack = value; }
+        }
+
+        //RequiredHours Property
+        public override double RequiredHours
+        {
+            get { return MATH_HOURS + CS_HOURS + GEN_HOURS; }
+        }
+    }
+}
